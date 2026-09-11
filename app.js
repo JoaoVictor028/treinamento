34d805497889569a41f9d3e757bcc82dd61c12ce
JoaoVictor028 <jvictorcunha@hotmail.com>
2026-09-11 11:35:38 -0300

    Feature 2

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ console.log("Commit 1");
 console.log("Commit 2");
 console.log("Commit 3");
 
-const msg = (msg) => console.log(msg);
-msg("Feat 01")
+const meuConsole = (msg, csl) => csl.log(msg);
+meuConsole("Feat 02", console);
